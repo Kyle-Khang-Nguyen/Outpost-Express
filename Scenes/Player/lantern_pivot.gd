1ef0c401c45c690681770c0ae8cf700d0bccf62a
EeebsieKat <eeebsthemodcat@gmail.com>
2026-01-12 17:37:32 -0700

    Added lots of stuff, tryna add items (packages) next

--- Scenes/Player/lantern_pivot.gd
+++ Scenes/Player/lantern_pivot.gd
@@ -1,15 +1,36 @@
 extends Node2D
 
-@export var max_angle := 18.0        # degrees
-@export var swing_speed := 8.0       # smoothing
-@export var sway_strength := 1.2     # movement influence
+@export var follow_speed := 8.0
+@export var max_distance := 40.0
 
-var target_angle := 0.0
+@export var bob_amount := 3.0
+@export var bob_speed := 2.0
 
-func _process(delta):
-	# Get player horizontal movement (adjust to your player script)
-	var move_x := Input.get_action_strength("right") \
-				- Input.get_action_strength("left")
+@export var target_path: NodePath
 
-	target_angle = move_x * max_angle * sway_strength
-	rotation = lerp(rotation, deg_to_rad(target_angle), swing_speed * delta)
+@onready var target: Node2D = get_node(target_path)
+
+var velocity := Vector2.ZERO
+var time := 0.0
+
+func _ready():
+	# IMPORTANT: start at the target
+	global_position = target.global_position
+
+func _physics_process(delta):
+	time += delta
+
+	var desired := target.global_position
+	var offset := desired - global_position
+
+	# --- critically damped spring ---
+	velocity = velocity.lerp(offset * follow_speed, 0.15)
+	global_position += velocity * delta
+
+	# --- hard clamp ---
+	if offset.length() > max_distance:
+		global_position = desired - offset.normalized() * max_distance
+		velocity = Vector2.ZERO
+
+	# --- gentle bob ---
+	global_position.y += sin(time * bob_speed) * bob_amount * delta
--- Scenes/Player/red.gd
+++ Scenes/Player/red.gd
@@ -4,13 +4,18 @@ extends CharacterBody2D
 
 @onready var sprite: Sprite2D = $PlayerSprite
 @onready var anim: AnimationPlayer = $AnimationPlayer
-@onready var lantern_pivot: Node2D = $LanternPivot
+@onready var lantern_pivot: Node2D = $LanternSide/LanternPivot
+
+@onready var lantern_side: Node2D = $LanternSide
+@export var lantern_side_offset: float = 16.0
+@export var lantern_height := -20.0
+@onready var lantern_target: Node2D = $LanternSide/LanternTarget
 
 #Lights
-@onready var lantern_light: PointLight2D = $LanternPivot/LanternSprite/LanternLight
-@onready var lantern_light_2: PointLight2D = $LanternPivot/LanternSprite/LanternLight2
-@onready var lantern_light_3: PointLight2D = $LanternPivot/LanternSprite/LanternLight3
-@onready var lantern_light_4: PointLight2D = $LanternPivot/LanternSprite/LanternLight4
+@onready var lantern_light: PointLight2D = $LanternSide/LanternPivot/LanternSprite/LanternLight
+@onready var lantern_light_2: PointLight2D = $LanternSide/LanternPivot/LanternSprite/LanternLight2
+@onready var lantern_light_3: PointLight2D = $LanternSide/LanternPivot/LanternSprite/LanternLight3
+@onready var lantern_light_4: PointLight2D = $LanternSide/LanternPivot/LanternSprite/LanternLight4
 
 # Colliders
 @onready var lantern_light_area_collider: CollisionShape2D = $LanternLightArea/LanternLightAreaCollider
@@ -35,6 +40,16 @@ func _process(_delta):
 
 
 func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact"):
+		for item in get_tree().get_nodes_in_group("DeliveryItems"):
+			if global_position.distance_to(item.global_position) < 16:
+				if not item.picked_up:
+					item.picked_up = true
+					item.holder = self
+				elif item.picked_up and item.holder == self:
+					if global_position.distance_to(item.delivery_spot.global_position) < 16:
+						item.queue_free()  # delivered
+						
 	character_direction.x = Input.get_axis("left", "right")
 	character_direction.y = Input.get_axis("up", "down")
 	character_direction = character_direction.normalized()
@@ -42,11 +57,13 @@ func _physics_process(delta: float) -> void:
 	# Sprite Flip
 	if character_direction.x > 0:
 		sprite.flip_h = false
-		lantern_pivot.scale.x = 1
+		lantern_target.position.x = lantern_side_offset
 	elif character_direction.x < 0:
 		sprite.flip_h = true
-		lantern_pivot.scale.x = -1
-		
+		lantern_target.position.x = -lantern_side_offset
+
+	lantern_target.position.y = lantern_height
+	
 	if character_direction:
 		velocity = character_direction * movement_speed
 		if !anim.is_playing() or anim.current_animation != "walk":
@@ -57,39 +74,3 @@ func _physics_process(delta: float) -> void:
 			anim.play("idle")
 			
 	move_and_slide()
-
-
-
-#@export var speed := 200.0
-#
-#@onready var anim: AnimationPlayer = $AnimationPlayer
-#
-#var last_dir := Vector2.DOWN
-#var current_anim := ""
-#
-#func _physics_process(delta: float) -> void:
-	#var direction: Vector2 = GameInputEvents.movement_input()
-	#
-	#if direction == Vector2.UP:
-		#anim.play("idle_up")
-	#elif direction == Vector2.RIGHT:
-		#anim.play("idle_right")
-	#elif direction == Vector2.DOWN:
-		#anim.play("idle_down")
-	#elif direction == Vector2.LEFT:
-		#anim.play("idle_left")
-	##else:
-		##anim.play("idle_down")
-		#
-	#if direction == Vector2.UP:
-		#anim.play("walk_up")
-	#elif direction == Vector2.RIGHT:
-		#anim.play("walk_right")
-	#elif direction == Vector2.DOWN:
-		#anim.play("walk_front")
-	#elif direction == Vector2.LEFT:
-		#anim.play("walk_left")
-	#
-	#velocity = direction * speed
-#
-	#move_and_slide()
