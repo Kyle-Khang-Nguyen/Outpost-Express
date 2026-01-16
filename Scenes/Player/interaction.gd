b870d16dc3aaa8a3d0328bf87a045278e4684f2e
DawningDew <dawningdew4@gmail.com>
2026-01-16 07:11:46 +0100

    Add rotation to interaction_raycast
    RayCast rotates with the player so it only interacts when you stand right next to the npc/object

--- Scenes/Player/interaction.gd
+++ Scenes/Player/interaction.gd
@@ -7,12 +7,14 @@ extends RayCast2D
 var last_direction : Vector2
 @onready var red: Player = $".."
 
+const raycast_length := 20
+
 func _physics_process(delta: float) -> void:
 	if red.character_direction != Vector2.ZERO:
 		last_direction = red.character_direction.normalized()
 		print(last_direction)
 	
-	interaction_ray_cast.target_position = last_direction * 10
+	interaction_ray_cast.target_position = last_direction * raycast_length
 	
 	# saves the colliding object
 	interaction_ray_cast.force_raycast_update()
