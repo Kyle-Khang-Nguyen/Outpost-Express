42bc056bdb637a9c39aa7657e12d029f0481fa9e
DawningDew <dawningdew4@gmail.com>
2026-01-17 18:25:34 +0100

    Added light_bulb mechanic

---
+++ Scenes/PuzzleRooms/PuzzleElements/light_bulb.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var light_bulb_on := false
+var warm = false
+
+func interact_with_player():
+	print(light_bulb_on)
+	print(warm)
+
+func _on_turn_switch_on():
+	light_bulb_on = true
+	warm = true
+	print("Received Signal")
+
+func _on_turn_switch_off():
+	light_bulb_on = false
+	print("turned bulb off")
--- Scenes/PuzzleRooms/PuzzleElements/light_switch.gd
+++ Scenes/PuzzleRooms/PuzzleElements/light_switch.gd
@@ -2,10 +2,16 @@ extends Area2D
 
 var turned_on = false
 @onready var light_switch: Node2D = $".."
-# stores the value whether it is the right switcher
-#var light_switch_value = light_switch.right_switch_value
 
 func interact_with_player():
+	if !turned_on:
 		print("Turned on")
 		turned_on = true
 		print("I am " + str(light_switch.right_switch_value))
+		
+		if light_switch.right_switch_value:
+			get_tree().call_group("LightSwitchReceiver", "_on_turn_switch_on")
+		else:
+			get_tree().call_group("LightSwitchReceiver", "_on_turn_switch_off")
+	else:
+		print("Switcher already switched once")
